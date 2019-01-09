abd3ecf145669ca3fed6fa5951001828aa9808c5
李盛雁 <lishengyan@lichengyandeiMac.local>
2019-01-09 17:46:53 +0800

    1

--- reporter.py
+++ reporter.py
@@ -1,1 +1,21 @@
 # coding=utf-8
+
+import common
+from log import Logger
+
+reporter_log_filename = "%s/%s" % (common.CONST_DIR_LOG, common.CONST_LOG_TRADER_FILENAME)
+
+
+class Reporter(object):
+    def __init__(self):
+        if not common.file_exist(common.CONST_DIR_LOG):
+            common.create_directory(common.CONST_DIR_LOG)
+
+        self.log = Logger(reporter_log_filename, level='debug')
+
+    def generate(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
--- trader.py
+++ trader.py
@@ -1,13 +1,18 @@
 # coding=utf-8
 
+import json
+
 import box
 import common
 from log import Logger
 
 trader_log_filename = "%s/%s" % (common.CONST_DIR_LOG, common.CONST_LOG_TRADER_FILENAME)
 trader_config_filename = "%s/%s" % (common.CONST_DIR_CONF, common.CONST_CONFIG_TRADER_FILENAME)
-trader_db_records_filename = "%s/%s" % (common.CONST_DIR_DATABASE, common.CONST_DB_TRADER_FILENAME)
+trader_db_records_filename = "%s/%s" % (common.CONST_DIR_DATABASE, common.CONST_DB_RECORDS_FILENAME)
+trader_db_position_filename = "%s/%s" % (common.CONST_DIR_DATABASE, common.CONST_DB_POSITION_FILENAME)
 trader_db_box_filename = box.box_db_filename
+
+MAX_VALID_BOX_INTERVAL_HOURS = 4  # 票箱会在每天的早上8：30，和中午12：00 左右开始选取，所以不会有操过
 
 
 class Trader(object):
@@ -26,17 +31,59 @@ class Trader(object):
     def _send_trader_records_mail(self):
         pass
 
-    def _load_box_db_file(self):
-        pass
+    @staticmethod
+    def _load_box_db_file():
+        if not common.file_exist(trader_db_box_filename):
+            return None, "stock box file is not exist."
 
-    def _load_position_db_file(self):
-        pass
+        try:
+            box_data_set = common.file_to_dict(trader_db_box_filename)
+        except Exception as err:
+            return None, err.message
 
-    def _save_position_db_file(self):
-        pass
+        box_timestamp = box_data_set.get("timestamp", None)
+        box_value = box_data_set.get("value", None)
+        if box_timestamp is None or box_value is None:
+            return None, "stock box data error, data is null."
 
-    def _load_trader_config(self):
-        pass
+        current_timestamp = common.get_current_timestamp()
+        if current_timestamp - box_timestamp > MAX_VALID_BOX_INTERVAL_HOURS:
+            return None, "stock box data is too old."
+
+        return box_value, None
+
+    @staticmethod
+    def _load_position_db_file():
+        if not common.file_exist(trader_db_position_filename):
+            return None, "position file is not exist."
+
+        try:
+            position_data_set = common.file_to_dict(trader_db_position_filename)
+            return position_data_set, None
+        except Exception as err:
+            return None, err.message
+
+    @staticmethod
+    def _save_position_db_file(data):
+        if common.file_exist(trader_db_position_filename):
+            common.delete_file(trader_db_position_filename)
+
+        try:
+            common.dict_to_file(data, trader_db_position_filename)
+        except Exception as err:
+            return err.message
+
+        return None
+
+    @staticmethod
+    def _load_trader_config():
+        if not common.file_exist(trader_config_filename):
+            return None, "config file: %s is not exist."
+        try:
+            with open(trader_config_filename, "r") as _file:
+                return json.load(_file), None
+        except Exception as err:
+            return None, err.message
 
     # 记录交易记录
     def _save_trader_records(self):
